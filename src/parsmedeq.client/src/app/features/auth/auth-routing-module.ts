e03852e6f45c16edc628c96ebee5c69a13f2cbf5
EhsaN <EhsaN.HaresAbady@gmail.com>
2025-08-08 12:24:29 +0330

    .

--- src/parsmedeq.client/src/app/features/auth/auth-routing-module.ts
+++ src/parsmedeq.client/src/app/features/auth/auth-routing-module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { Auth } from './auth';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {Login} from './login/login';
+import {Register} from './register/register';
+import {ForgotPassword} from './forgot-password/forgot-password';
 
-const routes: Routes = [{ path: '', component: Auth }];
+const routes: Routes = [
+  {path: '', component: Login},
+  {path: 'login', component: Login},
+  {path: 'register', component: Register},
+  {path: 'forgot-password', component: ForgotPassword},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {
+}
