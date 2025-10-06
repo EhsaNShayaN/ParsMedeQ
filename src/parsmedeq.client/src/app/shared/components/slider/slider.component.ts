63507927f2ed116a0f46282533fbdbb50e4a2cb0
EhsaNShayaN <EhsaN.HaresAbady@gmail.com>
2025-10-06 14:43:15 +0330

    slider

---
+++ src/parsmedeq.client/src/app/shared/components/slider/slider.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import 'swiper/element/bundle';
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss'],
+  standalone: false,
+})
+export class SliderComponent {
+}
